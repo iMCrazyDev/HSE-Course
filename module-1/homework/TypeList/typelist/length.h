0f4c4362baa238d48737b2affd1eaf24bbb9fbfe
Арсен <imcrazy.sixtouch@yandex.ru>
2020-12-21 23:32:20 +0500

    TypeList created

--- module-1/homework/TypeList/typelist/length.h
+++ module-1/homework/TypeList/typelist/length.h
@@ -2,5 +2,15 @@
 
 #include "typelist.h"
 
-template<typename TList> 
+template<typename TypeList>
 struct Length;
+
+template<typename Head, typename Tail>
+struct Length<TypeList<Head, Tail>> {
+	static constexpr int length = Length<Tail>::length + 1;
+};
+
+template<>
+struct Length<NullType> {
+	static constexpr int length = 0;
+};
